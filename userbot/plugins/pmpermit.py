15b12e1c1ecd829ab2fa02144db0faccdc4f750d
shoaibakhatar <55377721+shoaibakhatar@users.noreply.github.com>
2020-09-19 08:58:31 +0545

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -14,10 +14,10 @@ CACHE = {}
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No name set yet nibba, check pinned message in** @XtraTgBot"
 USER_BOT_WARN_ZERO = "`You were spamming my peru master's inbox, henceforth your retarded lame ass has been blocked by my master's userbot.` **Now GTFO, i'm playing minecraft** "
 USER_BOT_NO_WARN = ("[╔─━━━━━━━━🇳🇵░░  ░░🙄━━━━━━━━─╗\n                         🇳🇵S H O A I B 🇳🇵                            \n╚─━━━━━━━━🙏░░  ░░💩━━━━━━━━─╝](tg://user?id=1035034432)\n\n"
-                    "`Hello, NAMASTE🙏🙏🙏,  wait for my master.send your 3 msg onely,`"
+                    "`Hello, NAMASTE🙏🙏🙏,  wait for my master.send your 3 msg only,`"
                     f"{DEFAULTUSER}'s` inbox.\n\n"
-                    "😈😈agar clan member hai tyo ruk ja sab ko milega full help. 😡😡Nahi hai tyo nikal ja pahele first mai😡😡.`\n\n"
-                    "** Send file** `` ** 🇳🇵🇳🇵RUKO JARA RUKO😜😜.**")
+                    "😈😈Wait when I free provide full help. 😡😡send your msg donot spam😡😡.`\n\n"
+                    "** Send file or msg** `` ** 🇳🇵🇳🇵WAIT MAN WAIT MAN😜😜.**")
 
 
 if Var.PRIVATE_GROUP_ID is not None:
